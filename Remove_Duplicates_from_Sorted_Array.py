9e55c78c27eef766f8ad0d93f54330d6bf1a7111
annabechang <31103470+annabechang@users.noreply.github.com>
2019-03-19 12:37:13 +0800

    Create Remove_Duplicates_from_Sorted_Array.py

---
+++ Remove_Duplicates_from_Sorted_Array.py
@@ -0,0 +1,77 @@
+"""
+26. Remove Duplicates from Sorted Array
+Easy
+
+1396
+
+2940
+
+Favorite
+
+Share
+Given a sorted array nums, remove the duplicates in-place such that each element appear only once and return the new length.
+
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+Example 1:
+
+Given nums = [1,1,2],
+
+Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively.
+
+It doesn't matter what you leave beyond the returned length.
+Example 2:
+
+Given nums = [0,0,1,1,1,2,2,3,3,4],
+
+Your function should return length = 5, with the first five elements of nums being modified to 0, 1, 2, 3, and 4 respectively.
+
+It doesn't matter what values are set beyond the returned length.
+Clarification:
+
+Confused why the returned value is an integer but your answer is an array?
+
+Note that the input array is passed in by reference, which means modification to the input array will be known to the caller as well.
+
+Internally you can think of this:
+
+// nums is passed in by reference. (i.e., without making a copy)
+int len = removeDuplicates(nums);
+
+// any modification to nums in your function would be known by the caller.
+// using the length returned by your function, it prints the first len elements.
+for (int i = 0; i < len; i++) {
+    print(nums[i]);
+}
+"""
+
+
+
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        # count how many unique value
+        count = 0
+        
+        for i in range(len(nums)):
+            if nums[count] != nums[i]:
+                count += 1
+                #update nums of count
+                nums[count] = nums[i]
+        return count +1
+""" 
+        print ("The final nums before pop becomes ", nums)
+        for j in range(count+1, len(nums)):
+            nums.pop(-1)
+        print ("The final nums after pop becomes ", nums)
+        """   
+        
+        
+"""
+Success
+Details 
+Runtime: 60 ms, faster than 78.88% of Python3 online submissions for Remove Duplicates from Sorted Array.
+Memory Usage: 14.8 MB, less than 5.43% of Python3 online submissions for Remove Duplicates from Sorted Array.
+"""
