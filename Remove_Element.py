b7757839d2b8c18137304a43fde240a86b33e1c4
annabechang <31103470+annabechang@users.noreply.github.com>
2019-03-20 16:39:57 +0800

    Create Remove_Element.py

---
+++ Remove_Element.py
@@ -0,0 +1,70 @@
+"""
+27. Remove Element
+Easy
+
+746
+
+1617
+
+Favorite
+
+Share
+Given an array nums and a value val, remove all instances of that value in-place and return the new length.
+
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+
+Example 1:
+
+Given nums = [3,2,2,3], val = 3,
+
+Your function should return length = 2, with the first two elements of nums being 2.
+
+It doesn't matter what you leave beyond the returned length.
+Example 2:
+
+Given nums = [0,1,2,2,3,0,4,2], val = 2,
+
+Your function should return length = 5, with the first five elements of nums containing 0, 1, 3, 0, and 4.
+
+Note that the order of those five elements can be arbitrary.
+
+It doesn't matter what values are set beyond the returned length.
+Clarification:
+
+Confused why the returned value is an integer but your answer is an array?
+
+Note that the input array is passed in by reference, which means modification to the input array will be known to the caller as well.
+
+Internally you can think of this:
+
+// nums is passed in by reference. (i.e., without making a copy)
+int len = removeElement(nums, val);
+
+// any modification to nums in your function would be known by the caller.
+// using the length returned by your function, it prints the first len elements.
+for (int i = 0; i < len; i++) {
+    print(nums[i]);
+}
+"""
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        i, last = 0, len(nums) -1
+        
+        while i <= last:
+            if nums[i] == val:
+                nums[i], nums[last] = nums[last], nums[i]
+                last -= 1
+            else:
+                i += 1
+        
+        return last+1
+
+"""
+Success
+Details 
+Runtime: 40 ms, faster than 66.25% of Python3 online submissions for Remove Element.
+Memory Usage: 13.1 MB, less than 5.09% of Python3 online submissions for Remove Element.
+"""
