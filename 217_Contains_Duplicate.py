5281a130550dafcf1920a3b994912074faf2f2e0
annabechang <31103470+annabechang@users.noreply.github.com>
2019-07-10 13:36:26 +0800

    Create 217_Contains_Duplicate.py

---
+++ 217_Contains_Duplicate.py
@@ -0,0 +1,70 @@
+"""
+217. Contains Duplicate
+Easy
+
+423
+
+529
+
+Favorite
+
+Share
+Given an array of integers, find if the array contains any duplicates.
+
+Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
+
+Example 1:
+
+Input: [1,2,3,1]
+Output: true
+Example 2:
+
+Input: [1,2,3,4]
+Output: false
+Example 3:
+
+Input: [1,1,1,3,3,4,3,2,4,2]
+Output: true
+"""
+# dictionary
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        diction = {}
+        
+        for i in nums:
+            if i in diction:
+                return True
+            diction[i] = 1
+        return False
+"""
+Success
+Details 
+Runtime: 108 ms, faster than 67.35% of Python online submissions for Contains Duplicate.
+Memory Usage: 17.8 MB, less than 11.67% of Python online submissions for Contains Duplicate.
+"""
+#sort 
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        if len(nums) <= 1:
+            return False
+        else:
+
+            nums.sort()
+            for i in range(len(nums) - 1):
+                if nums[i] == nums[i+1]:
+                    return True
+            return False 
+"""
+Success
+Details 
+Runtime: 112 ms, faster than 44.71% of Python online submissions for Contains Duplicate.
+Memory Usage: 15.7 MB, less than 93.76% of Python online submissions for Contains Duplicate.
+"""
