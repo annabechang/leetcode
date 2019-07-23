7adcc68e24b6360fb4b343cefdbc2a7a268e54e6
annabechang <31103470+annabechang@users.noreply.github.com>
2019-07-23 17:20:16 +0800

    Create 232_Implement_Queue_using_Stacks.py

---
+++ 232_Implement_Queue_using_Stacks.py
@@ -0,0 +1,82 @@
+"""
+232. Implement Queue using Stacks
+Easy
+
+599
+
+112
+
+Favorite
+
+Share
+Implement the following operations of a queue using stacks.
+
+push(x) -- Push element x to the back of queue.
+pop() -- Removes the element from in front of queue.
+peek() -- Get the front element.
+empty() -- Return whether the queue is empty.
+Example:
+
+MyQueue queue = new MyQueue();
+
+queue.push(1);
+queue.push(2);  
+queue.peek();  // returns 1
+queue.pop();   // returns 1
+queue.empty(); // returns false
+Notes:
+
+You must use only standard operations of a stack -- which means only push to top, peek/pop from top, size, and is empty operations are valid.
+Depending on your language, stack may not be supported natively. You may simulate a stack by using a list or deque (double-ended queue), as long as you use only standard operations of a stack.
+You may assume that all operations are valid (for example, no pop or peek operations will be called on an empty queue).
+
+"""
+class MyQueue:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.stack = []
+
+    def push(self, x: int) -> None:
+        """
+        Push element x to the back of queue.
+        """
+        self.stack.append(x)
+
+    def pop(self) -> int:
+        """
+        Removes the element from in front of queue and returns that element.
+        """
+        ans = self.stack[0]
+        self.stack = self.stack[1:]
+        return ans
+
+    def peek(self) -> int:
+        """
+        Get the front element.
+        """
+        return self.stack[0]
+
+    def empty(self) -> bool:
+        """
+        Returns whether the queue is empty.
+        """
+        if len(self.stack): return False
+        return True
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
+
+"""
+Success
+Details 
+Runtime: 32 ms, faster than 83.78% of Python3 online submissions for Implement Queue using Stacks.
+Memory Usage: 13.7 MB, less than 5.55% of Python3 online submissions for Implement Queue using Stacks.
+"""
