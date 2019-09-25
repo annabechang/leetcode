6a881ad516c2c104d871fd02a46a3ea4b50528d6
annabechang <31103470+annabechang@users.noreply.github.com>
2019-09-25 17:13:30 +0800

    Create 268_missing_number.py

---
+++ 268_missing_number.py
@@ -0,0 +1,54 @@
+"""
+268. Missing Number
+Easy
+
+1104
+
+1508
+
+Favorite
+
+Share
+Given an array containing n distinct numbers taken from 0, 1, 2, ..., n, find the one that is missing from the array.
+
+Example 1:
+
+Input: [3,0,1]
+Output: 2
+Example 2:
+
+Input: [9,6,4,2,3,5,7,0,1]
+Output: 8
+Note:
+Your algorithm should run in linear runtime complexity. Could you implement it using only constant extra space complexity?
+"""
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        num_sum = sum(nums)
+        remain = (0+n)*(n+1)/2 - num_sum 
+        
+        
+        return int(remain)
+"""
+Success
+Details 
+Runtime: 156 ms, faster than 68.58% of Python3 online submissions for Missing Number.
+Memory Usage: 15 MB, less than 6.45% of Python3 online submissions for Missing Number.
+"""
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        
+        n = len(nums)
+        for i in range(n):
+            n = n^i^nums[i-1]
+        return n
+    
+
+"""
+Success
+Details 
+Runtime: 168 ms, faster than 26.04% of Python3 online submissions for Missing Number.
+Memory Usage: 15 MB, less than 6.45% of Python3 online submissions for Missing Number.
+"""
